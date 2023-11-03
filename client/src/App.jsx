1b9901cb03a26e7609d7725c359ac7e7c955ca83
Daniel Leonte <leontedaniel37@gmail.com>
2023-11-03 11:21:31 +0200

    reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RoomForm, RoomPage } from "./components";
 
 function App() {
-  // fetch("/api/room")
-  //   .then((response) => response.text())
-  //   .then((data) => console.log(data));
-
   return (
     <div className="flex justify-center items-center h-screen">
       <Router>
--- client/src/utils/socket.js
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.PROD ? undefined : "http://localhost:3000";
-
-export const socket = io(URL, { autoConnect: false });
